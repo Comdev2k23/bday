ec01f9c2f3d05a86a050130c16945ec9e7b66ad9
Comdev2k23 <comdev084@gmail.com>
2023-11-17 23:33:52 +0800

    4th commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-
+import Pic1 from '../assets/pic1.png'
+import Pic2 from '../assets/pic2.png'
+import Pic3 from '../assets/pic3.png'
+import Pic4 from '../assets/pic4.png'
+import Pic5 from '../assets/pic5.png'
+import Pic6 from '../assets/pic6.png'
 const Header = () => {
   return (
     <div className='mx-auto shadow-lg'>
-      <img src="./src/assets/pic1.png"  />
-      <img src="./src/assets/pic2.png"  />
-      <img src="./src/assets/pic3.png"  />
-      <img src="./src/assets/pic4.png"  />
-      <img src="./src/assets/pic5.png"  />
-      <img src="./src/assets/pic6.png"  />
+      <img src={Pic1}  />
+      <img src={Pic2}  />
+      <img src={Pic3} />
+      <img src={Pic4}  />
+      <img src={Pic5}  />
+      <img src={Pic6} />
     </div>
   )
 }
