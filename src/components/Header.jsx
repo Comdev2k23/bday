0f19cc3a049dad7d2ac4c4acea84462e612cc50e
Comdev2k23 <comdev084@gmail.com>
2023-11-17 23:26:26 +0800

    3rd commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='mx-auto shadow-lg'>
-      <img src="./src/assets/pic1.png" alt="" />
-      <img src="./src/assets/pic2.png" alt="" />
-      <img src="./src/assets/pic3.png" alt="" />
-      <img src="./src/assets/pic4.png" alt="" />
-      <img src="./src/assets/pic5.png" alt="" />
-      <img src="./src/assets/pic6.png" alt="" />
+      <img src="./src/assets/pic1.png"  />
+      <img src="./src/assets/pic2.png"  />
+      <img src="./src/assets/pic3.png"  />
+      <img src="./src/assets/pic4.png"  />
+      <img src="./src/assets/pic5.png"  />
+      <img src="./src/assets/pic6.png"  />
     </div>
   )
 }
