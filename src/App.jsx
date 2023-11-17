1029f4faa81f2aef3eed2d93299000613cee622b
Comdev2k23 <comdev084@gmail.com>
2023-11-17 23:02:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom"
+import Header from "./components/Header"
+function App() {
+ 
+  return (
+    <>
+        <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='mx-auto shadow-lg'>
+      <img src="src/assets/pic1.png" alt="" />
+      <img src="src/assets/pic2.png" alt="" />
+      <img src="src/assets/pic3.png" alt="" />
+      <img src="src/assets/pic4.png" alt="" />
+      <img src="src/assets/pic5.png" alt="" />
+      <img src="src/assets/pic6.png" alt="" />
+    </div>
+  )
+}
+
+export default Header
